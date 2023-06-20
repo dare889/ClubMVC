6a1ce8f1c046908c8bf82f80ccc0712b9f0a870f
dare889 <34261111+dare889@users.noreply.github.com>
2023-06-21 00:26:09 +0100

    ClubMVC first commit

---
+++ src/main/groovy/com/example/clubmvc/ClubmvcApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.clubmvc
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ClubmvcApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ClubmvcApplication, args)
+	}
+
+}
