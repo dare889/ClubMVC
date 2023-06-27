76b22707d10b231dd82dc5270baa57812b9afa08
dare889 <34261111+dare889@users.noreply.github.com>
2023-06-27 01:54:14 +0100

    Add some test case

--- src/test/groovy/com/example/clubmvc/ClubmvcApplicationTests.groovy
+++ src/test/groovy/com/example/clubmvc/ClubmvcApplicationTests.groovy
@@ -1,14 +1,21 @@
 package com.example.clubmvc
 
+import org.springframework.context.ApplicationContext
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ClubmvcApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
-		System.out.println("This is the testcase in this class");
+		// Verify that the application context is not null
+		assertThat(applicationContext).isNotNull();
 	}
 
 }
